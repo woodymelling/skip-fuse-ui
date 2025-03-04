88484884d7279ae55ddd561f68bbe9684ca4b54d
Abe White <abewhite@mac.com>
2025-03-04 16:10:58 -0600

    Make Mac build work

--- Sources/SkipFuseUI/Graphics/CoreGraphics.swift
+++ Sources/SkipFuseUI/Graphics/CoreGraphics.swift
@@ -1,6 +1,6 @@
 // Copyright 2025 Skip
 // SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
-#if !canImport(CoreGraphics)
+#if ROBOLECTRIC || !canImport(CoreGraphics)
 #if canImport(Darwin)
 import Darwin
 #elseif canImport(Android)
