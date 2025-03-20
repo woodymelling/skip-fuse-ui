969e20b871fd1888634be40e5909ef16d59d90f8
Marc Prud'hommeaux <marc@skip.tools>
2025-03-20 18:04:44 -0400

    Add explicit import of swift-jni to work around linker error

--- Package.swift
+++ Package.swift
@@ -11,12 +11,14 @@ let package = Package(
         .package(url: "https://source.skip.tools/skip.git", from: "1.2.21"),
         .package(url: "https://source.skip.tools/skip-fuse.git", from: "1.0.0"),
         .package(url: "https://source.skip.tools/skip-bridge.git", "0.0.0"..<"2.0.0"),
+        .package(url: "https://source.skip.tools/swift-jni.git", "0.0.0"..<"2.0.0"),
         .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0")
     ],
     targets: [
         .target(name: "SkipFuseUI", dependencies: [
             .product(name: "SkipFuse", package: "skip-fuse"),
             .product(name: "SkipBridge", package: "skip-bridge"),
+            .product(name: "SwiftJNI", package: "swift-jni"),
             .product(name: "SkipUI", package: "skip-ui")
         ], plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "SkipFuseUITests", dependencies: [
