5b56035f7d45711f107e6482ff258ed614d8c51e
Abe White <abewhite@mac.com>
2025-04-21 16:08:50 -0500

    Bridge onOpenURL

---
+++ Sources/SkipSwiftUI/System/UserActivity.swift
@@ -0,0 +1,31 @@
+// Copyright 2025 Skip
+// SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
+import Foundation
+import SkipUI
+
+extension View {
+    @available(*, unavailable)
+    /* nonisolated */ public func userActivity(_ activityType: String, isActive: Bool = true, _ update: @escaping (Any /* NSUserActivity */) -> ()) -> some View {
+        stubView()
+    }
+
+    @available(*, unavailable)
+    /* nonisolated */ public func userActivity<P>(_ activityType: String, element: P?, _ update: @escaping (P, Any /* NSUserActivity */) -> ()) -> some View {
+        stubView()
+    }
+
+    @available(*, unavailable)
+    /* nonisolated */ public func onContinueUserActivity(_ activityType: String, perform action: @escaping (Any /* NSUserActivity */) -> ()) -> some View {
+        stubView()
+    }
+
+    /* nonisolated */ public func onOpenURL(perform action: @escaping (URL) -> Void) -> some View {
+        return ModifierView(target: self) {
+            $0.Java_viewOrEmpty.onOpenURLString(perform: {
+                if let url = URL(string: $0) {
+                    action(url)
+                }
+            })
+        }
+    }
+}
