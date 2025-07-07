695f726e8f4e3aa0b99a4c349d463bf3938a6e2d
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-07-07 17:28:04 -0400

    Add Package@swift-5.10.swift

---
+++ Package@swift-5.10.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let android = Context.environment["TARGET_OS_ANDROID"] ?? "0" != "0"
+
+let package = Package(
+    name: "skip-fuse-ui",
+    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
+    products: [
+        .library(name: "SkipFuseUI", type: .dynamic, targets: ["SkipFuseUI"] + (android ? ["SwiftUI"] : [])),
+        .library(name: "SkipSwiftUI", type: .dynamic, targets: ["SkipSwiftUI"]),
+    ],
+    dependencies: [
+        .package(url: "https://source.skip.tools/skip.git", from: "1.5.24"),
+        .package(url: "https://source.skip.tools/skip-fuse.git", from: "1.0.2"),
+        .package(url: "https://source.skip.tools/skip-bridge.git", "0.14.1"..<"2.0.0"),
+        .package(url: "https://source.skip.tools/skip-android-bridge.git", "0.5.0"..<"2.0.0"),
+        .package(url: "https://source.skip.tools/swift-jni.git", "0.2.1"..<"2.0.0"),
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.29.3")
+    ],
+    targets: [
+        .target(name: "SkipFuseUI", dependencies: ["SkipSwiftUI"]),
+        .target(name: "SkipSwiftUI", dependencies: [
+            .product(name: "SkipFuse", package: "skip-fuse"),
+            .product(name: "SkipBridge", package: "skip-bridge"),
+            .product(name: "SkipAndroidBridge", package: "skip-android-bridge"),
+            .product(name: "SwiftJNI", package: "swift-jni"),
+            .product(name: "SkipUI", package: "skip-ui")
+        ], plugins: [.plugin(name: "skipstone", package: "skip")]),
+        .testTarget(name: "SkipSwiftUITests", dependencies: [
+            "SkipSwiftUI",
+            .product(name: "SkipTest", package: "skip")
+        ], plugins: [.plugin(name: "skipstone", package: "skip")]),
+    ]
+)
+
+if android {
+    package.targets += [.target(name: "SwiftUI", dependencies: ["SkipSwiftUI"])]
+}
